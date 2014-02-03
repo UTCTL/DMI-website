03cf6159cc69cd02fc7c78737fc4ce0d967d2de3
samueleishion <samueleishion@gmail.com>
2014-02-03 14:20:39 -0600

    Added support for automatic scrolling

--- _scrs/main.js
+++ _scrs/main.js
@@ -1,3 +1,25 @@
 $(document).ready(function() {
-	
+	setup_scrolling(); 
 }); 
+
+function setup_scrolling() {
+	$('nav .menu li').click(function(e) { 
+		e.preventDefault(); 
+		var id = $(this).attr('id'); 
+		if(id=="logo") id = "about"; 
+		scrollTo(id); 
+	}); 
+}
+
+function scrollTo(id) {
+	scrollAnimated(id,true,-1); 
+}
+
+function scrollAnimated(id,animation,actual) {
+	var qty = actual; 
+	if(actual<0) qty = $('section.'+id).offset().top-70; 
+
+	$('html,body').animate({ 
+		scrollTop: qty 
+	},1000,'swing'); 
+}
